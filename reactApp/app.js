76697f931ab1ea54d590e8ed2a6de5982e8ae7bc
Monkey <ole.orke@hotmail.com>
2018-07-26 14:50:52 -0700

    finish part 2

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<p>React lives here!</p>,
+var dummyData = ["Find meaning of life", "Walk the bird",
+"Balloon acupuncture", "Steal free samples", "Count all stars"]
+
+class TodoList extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <ul>
+        {dummyData.map((item) => <Todo task={item} />)}
+      </ul>
+    )
+  }
+}
+
+class Todo extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <li><button>X</button>{this.props.task}</li>
+    )
+  }
+}
+
+ReactDOM.render(<TodoList />,
   document.getElementById('root'));
